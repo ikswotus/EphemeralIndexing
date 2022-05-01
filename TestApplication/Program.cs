5ac477d63c9cae85bf41b246cdceac5faf8a25a4
ikswotus <jonsutowski@gmail.com>
2022-05-01 17:10:07 +0000

    Update Program.cs

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -18,90 +18,55 @@ namespace TestApplication
         static void Main(string[] args)
         {
 
+            string connString = args[0]; // System.IO.File.ReadAllText(@"E:\conn.txt");
+
+
             ILogger<EphemeralIndexingService.IndexingService> logger = GetLogger();
 
-            Demo(logger);
+            // Sets up initial indexing on some chunks
+            Demo(connString, logger);
 
-            logger.LogInformation("Demo complete - Press key to run Demo() again");
+            logger.LogInformation("Initial chunk indexing complete - Press key continue demo");
             Console.ReadKey();
 
-            Demo(logger);
+            Demo(connString, logger);
 
-            logger.LogInformation("Demo complete - Press key to run Demo(1)");
+            logger.LogInformation("Index check complete - Press key to continue demo and reduce indexing by adjusting retention");
             Console.ReadKey();
 
-            Demo(logger, 1);
+            Demo(connString, logger, 1);
 
-            logger.LogInformation("Demo complete - Press key to run Demo(8,true)");
+            logger.LogInformation("Retenion adjustment complete - Press key to continue demo and remove indexing on test hypertable");
             Console.ReadKey();
             
-            Demo(logger, 8, true);
+            Demo(connString, logger, 8, true);
 
 
-            logger.LogInformation("Finished");
-
-            //Demo();
-
-            //string connTest = System.IO.File.ReadAllText(@"E:\TODO\conn.txt");
-
-
-            //Dictionary<string, string> map =  EphemeralIndexing.IndexHelper.ChunkToRegularLimited(connTest, new List<string>() {"stats.timeseries_data_id", "test.simple_test"});
-            //foreach(string s in map.Keys)
-            //{
-            //    Console.WriteLine(s + "\t" + map[s]);
-            //}
-
-            ////string chunkName = "_hyper_0_1_chunk";
-            ////string indexName = "test_idx";
-
-            ////Console.WriteLine(EphemeralIndexing.IndexHelper.CreateIndex("test", chunkName, indexName, "machine_id"));
-
-            //ConfiguredOptions co = new ConfiguredOptions();
-            //co.Options.Add(new EphemeralIndexingOptions()
-            //{
-            //    Enabled = true,
-            //    AgeToIndex = TimeSpan.FromHours(4),
-            //    Hypertable = "demo.samples",
-            //    TimeColumn = "sample_time",
-            //    IndexCriteria = "sample_time,machine_id",
-            //    Predicate = null
-            //});
-
-            //OptionsHelper.ToFile(co, @"E:\TODO\options.xml");
-
-            //EphemeralIndexingService.ConfiguredOptions opts = OptionsHelper.FromFile(@"E:\TODO\options.xml");
-
-            //Console.WriteLine(opts.Options.Count.ToString());
-
-
-
+            logger.LogInformation("Demo Complete!");
         }
 
+        /// <summary>
+        /// Logging is a bit of a nightmare...get a console logger for use in the demo
+        /// </summary>
+        /// <returns></returns>
         public static ILogger<EphemeralIndexingService.IndexingService> GetLogger()
         {
-            // All this just to log to the console....thanks .net 5
+           
             var configureNamedOptions = new ConfigureNamedOptions<ConsoleLoggerOptions>("", null);
             var optionsFactory = new OptionsFactory<ConsoleLoggerOptions>(new[] { configureNamedOptions }, Enumerable.Empty<IPostConfigureOptions<ConsoleLoggerOptions>>());
             var optionsMonitor = new OptionsMonitor<ConsoleLoggerOptions>(optionsFactory, Enumerable.Empty<IOptionsChangeTokenSource<ConsoleLoggerOptions>>(), new OptionsCache<ConsoleLoggerOptions>());
             var loggerFactory = new LoggerFactory(new[] { new ConsoleLoggerProvider(optionsMonitor) }, new LoggerFilterOptions { MinLevel = LogLevel.Trace });
-
-            //ILoggerFactory loggerFactory = new LoggerFactory(
-            //              new[] { new ConsoleLoggerProvider(clo) });
-            ////or
-            ////ILoggerFactory loggerFactory = new LoggerFactory().AddConsole();
 
             ILogger<EphemeralIndexingService.IndexingService> logger = loggerFactory.CreateLogger<EphemeralIndexingService.IndexingService>();
             
             return logger;
         }
 
-        public static void Demo(ILogger<EphemeralIndexingService.IndexingService> logger, int hours = 4, bool clear = false)
+        public static void Demo(string connectionString, ILogger<EphemeralIndexingService.IndexingService> logger, int hours = 4, bool clear = false)
         {
-            // Create: TODO: Set connection string
-            string connStr = System.IO.File.ReadAllText(@"E:\conn.txt");
-
+            
             EphemeralIndexingService.ConfiguredOptions options = new ConfiguredOptions();
-            options.ConnectionString = connStr;
+            options.ConnectionString = connectionString;
 
             if (!clear)
             {
@@ -118,12 +83,6 @@ namespace TestApplication
             }
             // Save so service can find it
             EphemeralIndexingService.OptionsHelper.ToFile(options, System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "options.xml"));
-
-          
-
-
-            //  logger.LogInformation("This is log message.");
-
 
             EphemeralIndexingService.IndexingService service = new IndexingService(logger);
 
